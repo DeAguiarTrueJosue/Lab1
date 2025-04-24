0dff9ded44f621085722d557e25df8f57d483374
Josue Alessandro De Aguiar <josue.de_aguiar@lcieducation.net>
2025-04-23 17:35:19 -0700

    finishedWork

--- 3 - Task 3/main.cpp
+++ 3 - Task 3/main.cpp
@@ -1,29 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int main()
 {
-	int sum = 0;
-	int amount = 0;
-	int highest = 0;
-	int lowest = 100;
+	int highestScore = 0;
+	int lowestScore = 100;
+	float avarageScore = 0;
 	int perfectScores = 0;
+	int aboveAvarageScores = 0;
+	int amount = 0;
 
 	int numberEntered = 0;
 
-	printf("Welcome to a simple avarage calculator\n");
+	printf("This is your personal score analyser\n");
 
 	do
 	{
-		printf("Please, enter a positive number:");
+		printf("Please, enter a number between 1-100:");
 		scanf_s("%i", &numberEntered);
+		system("cls");
 
-		if (numberEntered > 0) {
-			sum += numberEntered;
+		if (numberEntered >= 0 && numberEntered <= 100) {
 			amount++;
-			system("cls");
-			printf("The last number you inputed was %i\n", numberEntered);
-			printf("If you want to stop, enter an invalid number");
+			if (numberEntered > highestScore)
+			{
+				highestScore = numberEntered;
+			}
+			if (numberEntered < lowestScore)
+			{
+				lowestScore = numberEntered;
+			}
+			if (amount != 0)
+			{
+				avarageScore = (avarageScore * (amount - 1) + numberEntered) / amount;
+			}
+			if (numberEntered == 100) { perfectScores++; }
+			if (numberEntered >= 70) { aboveAvarageScores++; }
+
+			printf("The number you entered was %i\n", numberEntered);
+			if (numberEntered >= 90)
+			{
+				printf("Excelent!\n");
+			}else if (numberEntered >= 70)
+			{
+				printf("Good!\n");
+			}else if (numberEntered >= 50)
+			{
+				printf("Avarage.\n");
+			}else
+			{
+				printf("Needs improvement.\n");
+			}
+			printf("Your highest score was %i\n", highestScore);
+			printf("Your lowest score was %i\n", lowestScore);
+			printf("Your avarage score was %.1f\n", avarageScore);
+			printf("The amount of perfect scores you got was %i\n", perfectScores);
+			printf("The amount of scores above avarage you got was %i\n", aboveAvarageScores); 
 
 		}
 		else {
@@ -31,16 +64,8 @@ int main()
 				printf("You need to inout at least one valid number\n");
 				numberEntered = 1;
 			}
-			else
-			{
-				printf("You entered a invalid number, the calculation has stopped\n");
-			}
 		}
 	} while (numberEntered > 0);
-
-	printf("You inputed %i numbers\n", amount);
-	printf("The sum of all inputed numbers is %i\n", sum);
-	printf("The avarage of all inputed numbers is %.2f\n", (float)sum / (float)amount);
 
 	return 0;
 }
